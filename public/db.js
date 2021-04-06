4eef7baabe316a1c1c5f68fc3b4b4cafbbe6f462
hapvskully <77029963+hapvskully@users.noreply.github.com>
2021-04-06 12:07:22 -0400

    database update

--- public/db.js
+++ public/db.js
@@ -1,14 +1,5 @@
 let db;
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://database-1/budget',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
 
 const request = window.indexedDB.open("budget", 1);
 
